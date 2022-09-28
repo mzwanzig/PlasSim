0e90cad9376a2ecb3c4110fb8c8683e92f885e06
Martin Zwanzig <52446235+mzwanzig@users.noreply.github.com>
2022-09-28 09:26:12 +0200

    Add files via upload

---
+++ launchPlasSim.R
@@ -0,0 +1,7 @@
+# The application resides in a directory ~/PlasSim/
+# The following packages are required:
+library(shiny) # first time? install.packages("shiny")
+library(deSolve)
+library(RColorBrewer)
+# and now launch the shiny app:
+runApp("PlasSim")
